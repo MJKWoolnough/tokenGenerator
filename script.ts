dbff94cb3c50707f3da476d3d48b16273f89988f
Michael Woolnough <michael.woolnough@gmail.com>
2022-01-31 15:31:43 +0000

    initial head creation (needs better chin)

--- script.ts
+++ script.ts
@@ -16,10 +16,14 @@ const labels = (() => {
 
 pageLoad.then(() => {
 	const update = () => {
+		const hW = parseInt(headWidth.value),
+		      hL = parseInt(headLength.value),
+		      hD = parseInt(headDepth.value);
+		amendNode(head, {"d": `M${1 + hW/10},${hL + (3 * hW + 4 * hD) / 8} q${-2 * hW / 10},${-hL / 5} ${-hW/3},${-hL/2} q${-hW/6},${-hL/2} ${-hW/6},${-(hL + hD) / 2} a3,2 0,0,1 ${hW},0 q0,${hD / 2} ${-hW/6},${(hL + hD) / 2} q${(-2 * hW) / 15},${3 * hL / 10} ${-hW/3},${hL/2}`});
 	      },
-	      headWidth = input({"type": "range", "min": 10, "max": 100, "value": 50, "onchange": update}),
-	      headLength = input({"type": "range", "min": 10, "max": 100, "value": 50, "onchange": update}),
-	      headDepth = input({"type": "range", "min": 10, "max": 100, "value": 50, "onchange": update}),
+	      headWidth = input({"type": "range", "min": 10, "max": 100, "value": 60, "onchange": update}),
+	      headLength = input({"type": "range", "min": 10, "max": 100, "value": 40, "onchange": update}),
+	      headDepth = input({"type": "range", "min": 10, "max": 100, "value": 40, "onchange": update}),
 	      head = path({"fill": "#fca", "stroke": "#000"}),
 	      s = svg({"viewBox": "0 0 1000 1000"}, head);
 	update();

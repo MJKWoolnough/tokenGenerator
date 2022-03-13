cebcc03c0478593c7d2559ca615e61571859a894
Michael Woolnough <michael.woolnough@gmail.com>
2022-03-13 16:50:57 +0000

    improved chin creation

--- script.ts
+++ script.ts
@@ -7,7 +7,7 @@ declare const pageLoad: Promise<void>;
 
 const labels = (() => {
         let next = 0;
-        return (name: Children, input: HTMLInputElement | HTMLButtonElement | HTMLTextAreaElement | HTMLSelectElement, props: Props = {}) => {
+        return (name: Children, input: HTMLInputElement | HTMLButtonElement | HTMLTextAreaElement | HTMLSelectElement, props: Exclude<Props, NamedNodeMap> = {}) => {
                 amendNode(input, {"id": props["for"] = `ID_${next++}`});
                 const l = label(props, name);
                 return [l, input];
@@ -16,19 +16,23 @@ const labels = (() => {
 
 pageLoad.then(() => {
 	const update = () => {
-		const hW = parseInt(headWidth.value),
+		const ckW = parseInt(cheekWidth.value),
+		      cnW = parseInt(chinWidth.value),
 		      hL = parseInt(headLength.value),
 		      hD = parseInt(headDepth.value);
-		amendNode(head, {"d": `M${1 + hW/10},${hL + (3 * hW + 4 * hD) / 8} q${-2 * hW / 10},${-hL / 5} ${-hW/3},${-hL/2} q${-hW/6},${-hL/2} ${-hW/6},${-(hL + hD) / 2} a3,2 0,0,1 ${hW},0 q0,${hD / 2} ${-hW/6},${(hL + hD) / 2} q${(-2 * hW) / 15},${3 * hL / 10} ${-hW/3},${hL/2}`});
+		amendNode(head, {"d": `M${1 + ckW},${hL + (3 * (2 * ckW + cnW) + 4 * hD) / 8} q${-2 * ckW / 5},${-hL / 5} ${-2 * ckW / 3},${-hL / 2} q${-ckW / 3},${-hL / 2} ${-ckW / 3},${-(hL + hD) / 2} a3,2 0,0,1 ${2 * ckW + cnW},0 q0,${hD / 2} ${-ckW / 3},${(hL + hD) / 2} q${-4 * ckW / 15},${3 * hL / 10} ${-2 * ckW / 3},${hL / 2} q${-cnW / 2},${cnW / 2} ${-cnW},0 ${-cnW},0 z`});
 	      },
-	      headWidth = input({"type": "range", "min": 10, "max": 100, "value": 60, "onchange": update}),
+	      cheekWidth = input({"type": "range", "min": 10, "max": 100, "value": 30, "onchange": update}),
+	      chinWidth = input({"type": "range", "min": 1, "max": 100, "value": 10, "onchange": update}),
 	      headLength = input({"type": "range", "min": 10, "max": 100, "value": 40, "onchange": update}),
 	      headDepth = input({"type": "range", "min": 10, "max": 100, "value": 40, "onchange": update}),
 	      head = path({"fill": "#fca", "stroke": "#000"}),
 	      s = svg({"viewBox": "0 0 1000 1000"}, head);
 	update();
 	clearNode(document.body, [
-		labels("Head Width: ", headWidth),
+		labels("Cheek Width: ", cheekWidth),
+		br(),
+		labels("Chin Width: ", chinWidth),
 		br(),
 		labels("Head Length: ", headLength),
 		br(),
